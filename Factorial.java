b6e1d39d662d0c070351e2481cb516a43b6ffe08
Himanshusing-h <himanshusingh1177q@gmail.com>
2026-06-23 08:07:01 +0530

    factorial

---
+++ Factorial.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number");
+        int n=sc.nextInt();
+        //calling function
+        System.out.println(factorial(n));
+    }
+    public static int factorial(int n) {
+        //base statement
+   if(n==0)
+   {
+       return 1;
+
+
+    }
+   //applying formula
+
+        int fac= factorial(n - 1) * n;
+   return fac;
+
+    }
+}
