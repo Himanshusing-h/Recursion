fee25b78f57d1c51ab65b0988fa48fd005b80954
Himanshusing-h <himanshusingh1177q@gmail.com>
2026-07-03 12:07:18 +0530

    decent

--- NoOfOccurance.java
+++ NoOfOccurance.java
@@ -1,5 +1,19 @@
 public class NoOfOccurance {
     public static void main(String[] args) {
-
+        System.out.println(occurance("character",'t',0,0));
+    }
+    //sochhhhh
+    //hume kya kya chahiye
+    //ek to string jisse check karenege;
+    //ek to char jisme  char ko store karenge compare karenge ke liye
+    //ek to index jo badhte rahega
+    //ek to count jo no of occurance count karega
+   static int occurance(String str,char ch,int ind,int count)
+    {
+        if(ind==str.length())return count;//base case
+        char sh=str.charAt(ind);//character at index nikalana
+        //abb do chize hongi ya to equal ya to not equal uske liye if else use kiya
+        if(sh==ch)return occurance(str,ch,ind+1,count+1);
+        else return occurance(str,ch,ind+1,count);
     }
 }
